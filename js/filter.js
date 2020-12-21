0dbea9b02ac7dbd1509f70386edb75d04ea3e750
Marta Cattaneo <martacattaneo@iMac-di-Marta.local>
2020-12-21 17:09:25 +0100

    mappa funzionante

--- js/filter.js
+++ js/filter.js
@@ -32,12 +32,12 @@ function init() {
     var mapElement = document.getElementById('haritam');
     var map = new google.maps.Map(mapElement, mapOptions);
     var locations = [
-        ['Location One', 'This is my address.', '0 111 222 33 44', 'info@mydomain.com', 'www.mydomain.com', 36.85915525936127, 30.79201858795159, 'http://www.karayeltasarim.com/Resim/Upload/osmark.png'],
-        ['Location Two', 'This is my address.', '0 242 344 10 20', 'info@mydomain.com', 'www.mydomain.com', 37.009614, 27.257248, 'http://www.karayeltasarim.com/Resim/Upload/osmark.png']
+        ['Location One', 'Description', 'img/before.jpg', 'info@mydomain.com', 'www.mydomain.com', 36.85915525936127, 30.79201858795159, 'https://www.cattaneomarta.it/wp-content/uploads/2020/12/osmark.png'],
+        ['Location Two', 'Description', 'img/after.jpg', 'info@mydomain.com', 'www.mydomain.com', 37.009614, 27.257248, 'https://www.cattaneomarta.it/wp-content/uploads/2020/12/osmark.png']
     ];
     for (i = 0; i < locations.length; i++) {
         if (locations[i][1] == 'undefined') { description = ''; } else { description = locations[i][1]; }
-        if (locations[i][2] == 'undefined') { telephone = ''; } else { telephone = locations[i][2]; }
+        if (locations[i][2] == 'undefined') { image = ''; } else { image = locations[i][2]; }
         if (locations[i][3] == 'undefined') { email = ''; } else { email = locations[i][3]; }
         if (locations[i][4] == 'undefined') { web = ''; } else { web = locations[i][4]; }
         if (locations[i][7] == 'undefined') { markericon = ''; } else { markericon = locations[i][7]; }
@@ -47,7 +47,7 @@ function init() {
             map: map,
             title: locations[i][0],
             desc: description,
-            tel: telephone,
+            img: image,
             email: email,
             web: web
         });
@@ -56,10 +56,10 @@ function init() {
         } else {
             link = web;
         }
-        bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+        bindInfoWindow(marker, map, locations[i][0], description, image, email, web, link);
     }
 
-    function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+    function bindInfoWindow(marker, map, title, desc, image, web, link) {
         var infoWindowVisible = (function() {
             var currentlyVisible = false;
             return function(visible) {
@@ -75,26 +75,24 @@ function init() {
                 iw.close();
                 infoWindowVisible(false);
             } else {
-                var html = "<div id='sube' style='color:#000;background-color:transparent;padding:5px;width:200px;'><h4>" + title + "</h4><p>" + desc + "<p><p>" + telephone + "<p><a href='mailto:" + email + "' >" + email + "<a><a href='" + link + "'' >" + web + "<a></div>";
+                var html = "<div style='color:#000;background-color:transparent;padding:2px;width:200px;'><h4>" + title + "</h4><p>" + desc + "<img src='" + image + "'' >" + "<a href='" + link + "'' >" + web + "</div>";
                 iw = new google.maps.InfoWindow({ content: html });
-                iw.open(map, marker, );
+                iw.open(map, marker);
                 infoWindowVisible(true);
             }
 
         });
+
+
         google.maps.event.addListener(iw, 'closeclick', function() {
             infoWindowVisible(false);
         });
 
-        google.maps.event.addListener(map, 'center_changed', function() {
-            window.setTimeout(function() {
-                map.panTo(marker.getPosition());
-            }, 1000);
-        });
 
         google.maps.event.addListener(marker, 'click', function() {
             map.setZoom(8);
             map.setCenter(marker.getPosition());
+
         });
     }
 }
